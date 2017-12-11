0f486b82c0122a5f6ba509dc32720294c9109fa8
CaptainT <captaintang@foxmail.com>
2017-12-11 15:18:15 +0800

    test o 640*480

--- MaskNet/MaskNet_test.py
+++ MaskNet/MaskNet_test.py
@@ -41,40 +41,46 @@ def getMask(fineSize, batchSize, nc, maskroot = None):
 
     return m
 
-def getMaskList(fineSize, batchSize, nc):
+def getMaskList(fineSize, batchSize, nc, root=None):
     from os import listdir
     from os.path import isfile, join
 
     folder = "masks256"
+    if root != None:
+        folder = root
+    catgry = 5
     cnt = 120
+
     onlyfiles = []
-    for i in range(cnt):
-        fname = "disparityMask1_" + str(i) + ".png"
-        onlyfiles.append(join(folder, fname))
+    for j in range(1, catgry + 1):
+        for i in range(cnt):
+            fname = "disparityMask" + str(j) + "_" + str(i) + ".png"
+            onlyfiles.append(join(folder, fname))
     # onlyfiles = [join(folder, f) for f in listdir(folder) if isfile(join(folder, f))]
 
     masklist = []
     for srcf in onlyfiles:
         mask = cv2.imread(srcf, cv2.IMREAD_GRAYSCALE)
-        if len(mask) != fineSize:
-            mask = cv2.resize(mask, (fineSize, fineSize))
+        if mask.shape != fineSize:
+            mask = cv2.resize(mask, fineSize)
+            print("change mask size")
 
         mask = torch.from_numpy(mask)
         mask = (mask == 255)
 
         mask = mask.type(torch.FloatTensor)
-        m = torch.FloatTensor(batchSize, nc, fineSize, fineSize)
 
-        for i in range(batchSize):
-            m[i, 0, :, :] = mask
-            m[i, 1, :, :] = mask
-            m[i, 2, :, :] = mask
-
-        masklist.append(m)
+        masklist.append(mask)
 
     return masklist
 
-def getImageList(fineSize, batchSize, nc, root=None):
+def fillBatchMask(maskUsed, oneChannel):
+    for i in range(len(maskUsed)):
+        for j in range(len(maskUsed[i])):
+            maskUsed[i][j] = oneChannel
+    return maskUsed
+
+def getImageList(height, width, batchSize, nc, root=None):
     from os.path import join
     from PIL import Image
     if batchSize != 1:
@@ -84,25 +90,28 @@ def getImageList(fineSize, batchSize, nc, root=None):
     if root == None:
         root = "images256"
 
+    catgry = 5
     cnt = 120
     onlyfiles = []
-    for i in range(cnt):
-        fname = "disparityImg1_"+str(i)+".png"
-        onlyfiles.append(join(root, fname))
+    for j in range(1, catgry + 1):
+        for i in range(cnt):
+            fname = "disparityImg"+ str(j) +"_"+str(i)+".png"
+            onlyfiles.append(join(root, fname))
     # onlyfiles = [join(folder, f) for f in listdir(folder) if isfile(join(folder, f))]
 
     trans = transforms.Compose([
-        transforms.Scale(fineSize),
-        transforms.CenterCrop(fineSize),
+        transforms.Scale(min(height, width)),
+        transforms.CenterCrop((height, width)),
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
     ])
+
 
     imglist = []
     for srcf in onlyfiles:
         img = Image.open(srcf).convert('RGB')
         m1 = trans(img)
-        m = torch.FloatTensor(batchSize, nc, fineSize, fineSize)
+        m = torch.FloatTensor(batchSize, nc, height, width)
         m[0] = m1
         imglist.append(m)
 
@@ -133,17 +142,20 @@ def saveOneImage(tensor, id, root=None):
 def main():
     # dataroot = "/home/cad/PycharmProjects/ContextEncoder/dataset/dining_room/d_val"
     # dataroot = '/home/cad/PycharmProjects/ContextEncoder/dataset/test128'
-    dataroot = "/home/cad/PycharmProjects/ContextEncoder/dataset/disparityTest"
+    dataroot = "/home/cad/PycharmProjects/ContextEncoder/dataset/disparityTest/test640"
     batchSize = 1
-    inputSize = 256
+    # inputSize = 256
+    height = 480
+    width = 640
     channel = 3
     ngpu = 2
 
     # load data
     dataset = dset.ImageFolder(root=dataroot,
                                transform=transforms.Compose([
-                                   transforms.Scale(inputSize),
-                                   transforms.CenterCrop(inputSize),
+                                   transforms.Scale(min(height, width)),
+                                   # transforms.Scale(inputSize),
+                                   transforms.CenterCrop((height, width)),
                                    transforms.ToTensor(),
                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                                ]))
@@ -154,26 +166,27 @@ def main():
     # mask = randomMask(inputSize, batchSize, channel)
     # mask = getMask(inputSize,batchSize, channel,
     #                maskroot="/home/cad/PycharmProjects/ContextEncoder/checkerMask6.png")
-    masklist = getMaskList(inputSize, batchSize, channel)
+    masklist = getMaskList((height, width), batchSize, channel,root="allMasks")
+    maskinUse = torch.FloatTensor(batchSize, channel, height, width)
 
 
     #load images
-    imglist = getImageList(inputSize, batchSize, channel)
+    imglist = getImageList(height, width, batchSize, channel, root=dataroot + "/t640")
 
     # load model
-    uModel = MASKNET(inputSize, inputSize, channel, nfeature=32, downTimes=5)
+    uModel = MASKNET(height, width, channel, nfeature=32, downTimes=5, stride=3)
     if ngpu:
         uModel = uModel.cuda()
         uModel = nn.DataParallel(uModel)
-    NetUroot = "checkpoints/Umodel_20.pth"
+    NetUroot = "checkpoints/Umodel_10.pth"
     uModel.load_state_dict(torch.load(NetUroot))
     uModel.eval()
 
-    dModel = MASKDNET(batch=batchSize, nc=channel, inputSize=inputSize, nf=32)
+    dModel = MASKDNET(batch=batchSize, nc=channel, width=width, height=height, nf=32)
     if ngpu:
         dModel = dModel.cuda()
         # !!! dModel = nn.DataParallel(dModel)
-    NetDroot = "checkpoints/dModel_20.pth"
+    NetDroot = "checkpoints/dModel_10.pth"
     dModel.load_state_dict(torch.load(NetDroot))
     dModel.eval()
 
@@ -206,10 +219,10 @@ def main():
         pass
 
     for i, data in enumerate(dataloader, 0):
-
         if 1:
             mask = masklist[i]
-            mask_gpu = Variable(mask).cuda()
+            maskinUse = fillBatchMask(maskinUse, mask)
+            mask_gpu = Variable(maskinUse).cuda()
 
         img_data, _ = data
 
@@ -218,7 +231,7 @@ def main():
 
         img_target = img_data.clone()
         img_target = Variable(img_target)
-        img_data = initData(img_data, mask)
+        img_data = initData(img_data, maskinUse)
         img_data = Variable(img_data)
         if ngpu:
             img_data = img_data.cuda()
@@ -263,7 +276,7 @@ def main():
                               'output/test_%d_fake.png' % (i),
                               nrow=nrow, normalize=True)
 
-        # vutils.save_image(img_fake.data, "result256/" + str(i) + ".png", normalize=True)
+        vutils.save_image(img_fake.data, "allResult/" + str(i) + ".png", normalize=True)
     if 1:
         fig, (ax0, ax1, ax2) = plt.subplots(ncols=3, figsize=(30, 10))
         ax0.plot(loss_img_list, label="$loss_img$")
@@ -279,6 +292,8 @@ def main():
         plt.legend()
         plt.savefig("test_G.png", dpi=200)
         plt.close()
+
+
 
     endT = time.localtime()
     print('begin: %d:%d:%d' % (beginT.tm_hour, beginT.tm_min, beginT.tm_sec))
